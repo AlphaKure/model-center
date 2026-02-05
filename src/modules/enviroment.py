4f4f2a5fe6a1253f2a0bae28201c5eb2fabb5cc1
alpha.wu <alpha6662017@outlook.com>
2026-02-05 17:54:55 +0800

    Fixed TorchInformation params mistake

--- src/modules/enviroment.py
+++ src/modules/enviroment.py
@@ -8,7 +8,7 @@ def torch_test() -> TorchInformation:
     """test torch available"""
     return TorchInformation(
         version= str(torch.__version__), # torch version
-        isAvailable= torch.cuda.is_available(),
-        deviceCount= torch.cuda.device_count(),
+        is_available= torch.cuda.is_available(),
+        device_count= torch.cuda.device_count(),
         devices = [torch.cuda.get_device_name(i) for i in range(0, torch.cuda.device_count())]
     )
